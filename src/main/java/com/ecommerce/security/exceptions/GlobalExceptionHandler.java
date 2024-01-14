c542b1dec878d04cedcf548e6be90e0eb3268a27
codekamit <kamitstar@icloud.com>
2024-01-14 21:04:37 +0530

    Authentication, Authorization service

--- src/main/java/com/ecommerce/security/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/security/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,11 @@
 package com.ecommerce.security.exceptions;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
+
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
